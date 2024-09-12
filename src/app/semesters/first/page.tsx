0f6f19484da4facdb764376eca8fb29762779d0b
shermuhammad786 <sherabro141@gmail.com>
2024-09-12 11:15:57 +0500

    changes

--- src/app/semesters/first/page.tsx
+++ src/app/semesters/first/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/app/components/footer/footer";
 import Header from "@/app/components/header/header";
 import Navbar from "@/app/components/navbar/page";
 
+
 export default function Home() {
     return (
         <div>
